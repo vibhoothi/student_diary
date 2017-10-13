d0f7fb9c8cedee2947d8941685d5c58744ee4a44
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-10-13 22:06:23 +0530

    student:functions:newStudent() and printDetails()

--- src/student_diary/student.java
+++ src/student_diary/student.java
@@ -1,5 +1,6 @@
 package student_diary;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 public class student {
 	String name;
@@ -10,7 +11,7 @@ public class student {
 		name="";
 		rollNo="";
 	    Calendar c = Calendar.getInstance();
-	    c.set(2016,1);
+	    c.set(2016,1,1);
 		dob=c.getTime();	
 	}
 	public student(String name,String rollNo,String campus,Date dob) {
@@ -30,5 +31,29 @@ public class student {
 	}
 	public String getCampus() {
 		return campus;
+	}
+	public void newStudent() {
+		System.out.println("Enter name");
+		Scanner s= new Scanner(System.in);
+		this.name=s.next();
+		this.rollNo=s.next();
+		System.out.println("Enter DOB(YYYY/MM/DD) order");
+		int year,month,day;
+		year=s.nextInt();
+		month=s.nextInt();
+		day=s.nextInt();
+		Calendar ca=Calendar.getInstance();
+		ca.set(year, month, day);
+		s.close();
+	}
+	public void printDetails() {
+	 System.out.println("Name"+name);
+	 System.out.println("rollNo"+name);	
+	 System.out.println("campus"+name);
+	 SimpleDateFormat date =new SimpleDateFormat("dd/MM/yyyy");
+	 System.out.println("DOB"+date.format(dob));
+	 
+	 
+	 
 	}
 }
