98b2cb0c2b6e9e3664d45cdd38acc37253a65a79
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-10-15 14:36:10 +0530

    student.java:Few additions This commits include the following changes New inclusion:  -Gender  -semester  -branch

--- src/student_diary/student.java
+++ src/student_diary/student.java
@@ -7,18 +7,27 @@ public class student {
 	String rollNo;
     String campus;
 	Date dob;
+	String gender;
+	int semester;
+	String branch;
 	public student() {
 		name="";
 		rollNo="";
 	    Calendar c = Calendar.getInstance();
 	    c.set(2016,1,1);
 		dob=c.getTime();	
+		gender=" ";
+		semester=1;
+		branch=" ";
 	}
-	public student(String name,String rollNo,String campus,Date dob) {
+	public student(String name,String rollNo,String campus,Date dob,String gender,int semester,String branch) {
 		this.name=name;
 		this.rollNo=rollNo;
 		this.campus=campus;
 		this.dob=dob;
+		this.gender=gender;
+		this.semester=semester;
+		this.branch=branch;
 	}
 	public String getName() {
 		return name;
@@ -32,11 +41,24 @@ public class student {
 	public String getCampus() {
 		return campus;
 	}
+	public String getGender() {
+		return gender;
+	}
+	public int getSemester() {
+		return semester;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	
 	public void newStudent() {
-		System.out.println("Enter name");
 		Scanner s= new Scanner(System.in);
 		this.name=s.next();
 		this.rollNo=s.next();
+		this.branch=s.next();
+		this.gender=s.next();
+		this.semester=s.nextInt();
+		this.campus=s.next();
 		System.out.println("Enter DOB(YYYY/MM/DD) order");
 		int year,month,day;
 		year=s.nextInt();
@@ -50,6 +72,9 @@ public class student {
 	 System.out.println("Name"+name);
 	 System.out.println("rollNo"+name);	
 	 System.out.println("campus"+name);
+	 System.out.println("Branch:"+branch);
+	 System.out.println("Semester"+semester);
+	 System.out.println("gender:"+branch);
 	 SimpleDateFormat date =new SimpleDateFormat("dd/MM/yyyy");
 	 System.out.println("DOB"+date.format(dob));
 	 
