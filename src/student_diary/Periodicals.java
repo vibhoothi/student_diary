3d91c4df89cb0d9b493159c040223d6606cac080
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-11-04 21:51:46 +0530

    Periodicals:Add a class to input and display user about the Periodicals

---
+++ src/student_diary/Periodicals.java
@@ -0,0 +1,30 @@
+package student_diary;
+
+import java.util.Scanner;
+
+public  class Periodicals implements Marks {
+	int p1Marks;
+	int p2Marks;
+	Periodicals(){
+		p1Marks=0;
+		p2Marks=0;	
+	}
+	public int getMarks() {
+		return p1Marks+p2Marks;
+	}
+	public void setMarks(int p1,int p2) {
+		p1Marks=p1;
+		p2Marks=p2;
+	}
+	public void addMark() {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter Periodical 1 marks");
+		p1Marks=s.nextInt();
+		System.out.println("Enter Periodical 2 Marks");
+		p2Marks=s.nextInt();
+	}
+	public void display() {
+		System.out.println("First Periodical:"+p1Marks);
+		System.out.println("Second Periodical :"+p2Marks);
+	}
+}
