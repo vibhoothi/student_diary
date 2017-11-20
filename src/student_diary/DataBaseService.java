f14edf1803efe08f043acbf490296d9f5b6605d3
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-11-21 02:20:48 +0530

    DataBaseService:Make a DataBase service.  In this program we uses database service  DataBaseService(DBS) where all the things  are stored so you just need to call that  DBS so all the fields are created in it The main Advantage of using this is you  can easily add new feature, find bugs  delete functions

---
+++ src/student_diary/DataBaseService.java
@@ -0,0 +1,13 @@
+package student_diary;
+import java.io.*;
+import java.util.*;
+
+public class DataBaseService implements Serializable  {
+	
+	Student studentData =new Student();
+	Events eventData = new Events();
+	Periodicals periodicalsData = new Periodicals();
+	Hostel hostelData =new Hostel();
+	Attendance attendanceData =new Attendance(); 
+	ParentDetails parentData =new ParentDetails();
+}
