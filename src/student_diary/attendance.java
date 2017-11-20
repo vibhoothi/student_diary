30489962e3917a65286dd32f2ef4e7b954f3fda6
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-11-21 02:17:53 +0530

    Attendance:Final Attendance class

--- src/student_diary/attendance.java
+++ src/student_diary/attendance.java
@@ -1,17 +1,31 @@
 package student_diary;
 
-public class attendance {
-	String date;
-	int att;
-	public attendance() {
-		date=" ";
-		att=0;
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Attendance implements Serializable {
+	int attend;
+	int totalNoOfHours;
+	
+	public Attendance() {
+		attend=1;
+		totalNoOfHours=1;
 	}
-	public void addAtt(String date) {
-		att=1;
+	public void getAttendance() {
+		 Scanner s = new Scanner(System.in);
+		 System.out.println("Enter no of hours attented");
+		 attend=s.nextInt();
+		 System.out.println("Enter Total no of hours");
+		 totalNoOfHours=s.nextInt();
 	}
-	public void print() {
-		System.out.println(" "+ date +":"+att);
+	
+	int finalAttend=0;
+	//finalAttend=attend/totalNoOfHours;	
+	
+	public void showAttend() {
+		System.out.println("Total Number of hours Attended is"+attend);
+		System.out.println("Total Number of hours"+totalNoOfHours);
+		System.out.println("Final Attendance"+finalAttend);
 	}
 
 }
