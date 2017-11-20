12a23bcce0cc18e4869a1e3b4617fec730bc1694
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-11-21 02:21:19 +0530

    Events:Final few changes

--- src/student_diary/Events.java
+++ src/student_diary/Events.java
@@ -1,23 +1,27 @@
 package student_diary;
+import java.io.Serializable;
 import java.util.Scanner;
-public class events {
+public class Events implements Serializable {
 	String eventName;
 	String prize;
 	String place;
-	public events() {
-		eventName=" ";
-		prize=" ";
-		place=" ";
+	public Events() {
+		eventName="Test ";
+		prize="Tset ";
+		place=" Test";
 	}
 	public void addEvent() {
 		 Scanner s = new Scanner(System.in);
-		 this.eventName = s.next();
-	     this.prize = s.next();
-	     this.place=s.next();
+		 System.out.println("Enter Event Name");
+		 eventName = s.next();
+		 System.out.println("Enter Prize");
+	     prize = s.next();
+	     System.out.println("Enter Place of event");
+	     place=s.next();
 	}
 	public void display() {
-		System.out.println("Event Name:"+eventName);
-		System.out.println("Prize:"+prize);
-		System.out.println("Place"+place);
+		System.out.println("Event Name :"+eventName);
+		System.out.println("Prize :"+prize);
+		System.out.println("Place :"+place);
 	}
 }
